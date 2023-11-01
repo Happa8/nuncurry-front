aa11355b8ef47e3fbee6191bea4502d1e42531a8
Happa8 <happa.eight@gmail.com>
2023-11-01 11:12:23 +0900

    setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { css } from "../styled-system/css";
+
+const App = () => {
+  return (
+    <>
+      <div className={css({ margin: 10 })}>test</div>
+    </>
+  );
+};
+
+export default App;
